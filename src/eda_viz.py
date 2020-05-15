e5b6096496b43fa99c3b29d0125c09eab9c60616
Tommy Evans-Barton <tevansbarton@tommys-mbp.attlocal.net>
2020-05-15 02:17:12 -0700

    Some last little changes

--- src/eda_viz.py
+++ src/eda_viz.py
@@ -10,7 +10,7 @@ DATA_OUTPATH = TOP_PATH + '/eda_visualizations'
 
 
 def month_race_count_viz(date_range, issue, save_fig = False):
-    month_dictionary = {
+    MONTH_DICTIONARY = {
     1 : 'Jan',
     2 : 'Feb',
     3 : 'Mar',
@@ -28,18 +28,23 @@ def month_race_count_viz(date_range, issue, save_fig = False):
     df_window = get_data_from_range(date_range)
     #Select out the columns that we are focused on in this project
     df_window = df_window[['stop_id', 'service_area', 'subject_race', 'date_stop']]
+    #Make month column using string operations on the date
     df_window['Month'] = df_window['date_stop'].str.slice(5,7)
     df_window = df_window.astype({'Month' : int})
+    #Make a pivot table: gets counts of stops for each race for each month
     viz_df = df_window.pivot_table(values = 'stop_id', index = 'Month', columns = 'subject_race', aggfunc = 'count')
-    viz_df.index = viz_df.index.map(month_dictionary)
+    viz_df = viz_df.sort_values('Month')
+    viz_df.index = viz_df.index.map(MONTH_DICTIONARY)
+    #Create a stacked bar chart
     ax = viz_df.plot.bar(stacked = True, figsize = [12, 6.75])
     ax.set(title = 'Counts of Race Stops in Window Around {iss} by Month'.format(iss = issue),
            ylabel = 'Individuals Stopped')
     fig = ax.figure
+    #If save figure is passed, save the image to disk
     if save_fig:
         if not os.path.exists(DATA_OUTPATH):
             os.makedirs(DATA_OUTPATH, exist_ok = True)
         fig.savefig(DATA_OUTPATH + '/{iss}_RACE_BY_MONTH_DISTR.png'.format(iss = issue))
-    return
+    return ax
 
 
